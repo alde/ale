6cb8b7c427600c4f86b259e790bbaa12417fef2c
Rickard Dybeck <dybeck@spotify.com>
2019-02-26 17:53:44 +0100

    Helps if you don't ignore test errors

--- jenkins/crawler_test.go
+++ jenkins/crawler_test.go
@@ -54,15 +54,15 @@ func Test_SplitLogs(t *testing.T) {
 	input := "<span class=\"timestamp\"><b>15:38:12</b> </span><span style=\"display: none\">[2019-02-14T15:38:12.376Z]</span> [WS-CLEANUP] Deleting project workspace...\n<span class=\"timestamp\"><b>15:38:12</b> </span><span style=\"display: none\">[2019-02-14T15:38:12.376Z]</span> [WS-CLEANUP] Deferred wipeout is used...\n<span class=\"timestamp\"><b>15:38:12</b> </span><span style=\"display: none\">[2019-02-14T15:38:12.381Z]</span> [WS-CLEANUP] done\n"
 	expected := []*ale.Log{
 		{
-			TimeStamp: "15:38:12",
+			TimeStamp: "2019-02-14T15:38:12.376Z",
 			Line:      "[WS-CLEANUP] Deleting project workspace...",
 		},
 		{
-			TimeStamp: "15:38:12",
+			TimeStamp: "2019-02-14T15:38:12.376Z",
 			Line:      "[WS-CLEANUP] Deferred wipeout is used...",
 		},
 		{
-			TimeStamp: "15:38:12",
+			TimeStamp: "2019-02-14T15:38:12.381Z",
 			Line:      "[WS-CLEANUP] done",
 		},
 	}
